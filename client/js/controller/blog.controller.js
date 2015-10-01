f865d6e2ef037308e09b6b2fc6ab7e94e924ca78
Jay Blake <jayblake@Jays-MacBook-Air.local>
2015-09-30 21:47:00 -0500

    almost done, a few input validation bugs and need a blog array set up in rootscope

--- client/js/controller/blog.controller.js
+++ client/js/controller/blog.controller.js
@@ -1,4 +1,5 @@
 BlogSite.controller('blogpostsController', ['$scope', '$http', '$rootScope', '$window', 'Parse', function($scope, $http, $rootScope, $window, Parse) {
+
 	Parse.getPosts()
 	.then(function(res) {
 		console.log(res);
@@ -7,12 +8,15 @@ BlogSite.controller('blogpostsController', ['$scope', '$http', '$rootScope', '$w
 		console.log(err);
 		alert('There was an error!');
 	});
+
 	$scope.createBlogEntry = function() {
 		$window.location.href = '#newpost';
 	}
+
 }]);
 
 BlogSite.controller('newpostController', ['$scope', '$http', '$rootScope', '$window', 'Parse', function($scope, $http, $rootScope, $window, Parse) {
+
 	$scope.newPost = function() {
 		var blogAuthor = $scope.blogAuthor, blogTitle = $scope.blogTitle, blogContent = $scope.blogContent;
 		if ( blogAuthor === '' || blogTitle === '' || blogContent === '' ) {
@@ -33,49 +37,5 @@ BlogSite.controller('newpostController', ['$scope', '$http', '$rootScope', '$win
 			})
 		}
 	}
+
 }]);
-
-// TweetApp.controller('tweetController', ['$scope', '$http', '$rootScope', '$window', function($scope, $http, $rootScope, $window) {
-
-// 	$scope.userName = $rootScope.userName;
-// 	if ( $scope.userName === '' || ! $scope.userName ) {
-// 		$scope.userName = 'Anonymous';
-// 	}
-// 	$scope.tweets = [];
-// 	var urlPath = 'http://localhost:3000/messages';
-	
-// 	$http.get(urlPath)
-// 	.then(function(res) {
-// 		$scope.tweets = res.data.reverse();
-// 	}).catch(function(err) {
-// 		console.log(err);
-// 		alert('Something horrible happened!');
-// 	});
-	
-// 	$scope.postTweet = function() {
-// 		var tweetObj = { text: $scope.tweetText, userName: $scope.userName };
-// 		$http.post(urlPath, JSON.stringify(tweetObj))
-// 		.then(function(res) {
-// 			$scope.tweetText = '';
-// 			return $http.get(urlPath);
-// 		}).then(function(res) {
-// 			$scope.tweets = res.data.reverse();
-// 		}).catch(function(err) {
-// 			console.log(err);
-// 			alert('Something horrible happened!');
-// 		});
-// 	};
-// }]);
-
-// TweetApp.controller('welcomeController', ['$scope', '$http', '$rootScope', '$window', function($scope, $http, $rootScope, $window) {
-
-// 	$scope.setUser = function() {
-// 		if ( $scope.userName === ''  || ! $scope.userName ) {
-// 			$rootScope.userName = 'Anonymous';
-// 		} else {
-// 			$rootScope.userName = $scope.userName;
-// 		}
-// 		$window.location.href = '#tweets';
-// 	}
-
-// }]);
